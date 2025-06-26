e3239f5cb1033ff1789d87b1f26303ca3202db54
pym9539 <pym9539@naver.com>
2025-06-26 11:15:29 +0900

    웹서버에 유튜브 올림

---
+++ humortube/src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+
+  const apple = 1
+
+  return ( 
+  <div>
+    <h1>유튜브모음</h1>
+    <br/>리버풀
+    <hr />
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/YMAgU5oA_-I?si=G6Wg6FKB9PhtDiXz" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    <br/><br/>
+    <p>FIFA CLUBWORLDCUP</p>
+    <hr />
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/Le1JPvEcUuQ?si=r0qo9YtsQuR-opsN" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/k4BoUmolDS0?si=LsfJP5fYtZCWGY5y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+  </div>
+  )
+}
+
+export default App
---
+++ test.js
@@ -0,0 +1,9 @@
+let a = 10;
+let b = 20;
+let c = a+b;
+
+//c의 값 보여주기
+
+
+
+console.log(c)
