54497c7deee96b7573ea0283fe8c6b7877b584e4
pym9539 <pym9539@naver.com>
2025-06-27 11:02:23 +0900

    did 작업용

---
+++ pym/src/App.jsx
@@ -0,0 +1,32 @@
+import {useState} from 'react';
+
+function App() {
+  const [did_time, set_did_time] = useState("");
+
+  function getdate()
+  {
+    const date = new Date();    
+    return date.toLocaleDateString('ko-KR') + ' ' +  date.toLocaleTimeString('ko-KR');
+  }
+
+  function checkTime()
+  {
+    let current_time = getdate()
+    set_did_time(current_time)
+  }
+
+  return (
+    <>
+      <div className="center_div">
+        <div>
+          <button onClick={checkTime}>출/퇴근</button>
+        </div>
+        <div>
+          {did_time}
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
